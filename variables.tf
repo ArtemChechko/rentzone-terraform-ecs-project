bc29b33d085f588f0cec475ebb9538721daf937f
Artem Chechko <chechkoartem87@gmail.com>
2024-08-06 21:58:58 +0300

    created nat-gateway file

--- variables.tf
+++ variables.tf
@@ -22,28 +22,28 @@ variable "vpc_cidr" {
 }
 
 variable "public_subnet_az1_cidr" {
-  description = "public subnet az1 cidr block"
-  type = string
+  description   = "public subnet az1 cidr block"
+  type          = string
 }
 
 variable "public_subnet_az2_cidr" {
-  description = "public subnet az2 cidr block"
-  type = string
+  description   = "public subnet az2 cidr block"
+  type          = string
 }
 
 variable "private_app_subnet_az1_cidr" {
-  description = "private app subnet az1 cidr block"
-  type = string
+  description   = "private app subnet az1 cidr block"
+  type          = string
 }
 
 variable "private_app_subnet_az2_cidr" {
-  description = "private app subnet az2 cidr block"
-  type = string
+  description   = "private app subnet az2 cidr block"
+  type          = string
 }
 
 variable "private_data_subnet_az1_cidr" {
-  description = "private data subnet az1 cidr block"
-  type = string
+  description   = "private data subnet az1 cidr block"
+  type          = string
 }
 
 variable "private_data_subnet_az2_cidr" {
